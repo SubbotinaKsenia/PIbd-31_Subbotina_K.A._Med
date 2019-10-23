b1610de9aae3583da473d1bb9c3ee0c7010d54a8
Ksenia <lelsene@mail.ru>
2019-10-23 22:44:10 +0400

    edit package.json, doctors.service.ts

--- src/app/admin/doctors.service.ts
+++ src/app/admin/doctors.service.ts
@@ -16,36 +16,38 @@ export class DoctorsService {
         this.handleError = httpErrorHandler.createHandleError('DoctorsService')
     }
 
+    link: string = 'https://ipmedbackend.herokuapp.com/';
+
     getDoctors(): Observable<any> {
         return this.http
-            .get('api/doctors')
+            .get(this.link + 'api/doctors')
             .pipe(catchError(this.handleError('getDoctors')))
     }
 
     getDoctor(id: number): Observable<any> {
         const url = 'api/doctors/' + id;
         return this.http
-            .get(url)
+            .get(this.link + url)
             .pipe(catchError(this.handleError('getDoctor', id)))
     }
 
     addDoctor(doctor: Doctor): Observable<any> {
         return this.http
-            .post('api/doctors', doctor)
+            .post(this.link + 'api/doctors', doctor)
             .pipe(catchError(this.handleError('addDoctor', doctor)))
     }
 
     deleteDoctor(id: number): Observable<any> {
         const url = 'api/doctors/' + id;
         return this.http
-            .delete(url)
+            .delete(this.link + url)
             .pipe(catchError(this.handleError('deleteDoctor', id)))
     }
 
     updateDoctor(doctor: Doctor): Observable<any> {
         const url = 'api/doctors/' + doctor.id;
         return this.http
-            .put(url, doctor)
+            .put(this.link + url, doctor)
             .pipe(catchError(this.handleError('updateDoctor', doctor)))
     }
 }
